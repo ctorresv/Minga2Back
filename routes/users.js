3363ea582f2eb228bb80f088ae1daeb384a060d4
CATorres Varon <44732360+ctorresv@users.noreply.github.com>
2023-07-18 11:56:52 -0500

    Creacion signup (#1)

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ import signin from '../controllers/users/signin.js';
 import accountExistsSignIn from '../middlewares/accountExistsSignIn.js';
 import passwordIsOk from '../middlewares/passwordIsOk.js'
 import accountHasBeenVerified from '../middlewares/accountHasBeenVerified.js';
+import accountExistsSignUp from '../middlewares/accountExistsSignUo.js';
+import signUp from '../controllers/users/signup.js';
 const router = express.Router();
 
 /* GET users listing. */
@@ -10,6 +12,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signin',accountExistsSignIn,passwordIsOk,accountHasBeenVerified,signin)
+router.post('/signin', accountExistsSignIn,passwordIsOk,accountHasBeenVerified,signin)
+router.post("/signup", accountExistsSignUp, signUp)
 
 export default router;
